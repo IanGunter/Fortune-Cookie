b5a147ba9ba1eb102af8b21c3ecea7b9ca34461a
Ian Gunter <iangunterofamerica@yahoo.com>
2020-09-12 23:49:21 -0600

    Add Option Strict Add Option Compare Text Add Option Explicit

--- FortuneCookie.vb
+++ FortuneCookie.vb
@@ -4,6 +4,10 @@
 'Fall 2020
 'https://github.com/IanGunter/Fortune-Cookie.git
 
+Option Strict On
+Option Compare Text
+Option Explicit On
+
 
 Module FortuneCookie
 
@@ -41,11 +45,11 @@ Module FortuneCookie
     End Sub
 
     'Function Below creates a randomly generated number between 1 and 3.
-    Function GetRandomNumber(ByVal minimum As Integer, ByVal maximum As Integer)
-        Dim value As Integer
+    Function GetRandomNumber(ByVal minimum As Integer, ByVal maximum As Integer) As Integer
+        Dim value As Single
         'Rnd picks a number between 0 and 1.
         value = ((maximum - minimum + 1) * Rnd()) + minimum
-        Return value
+        Return CInt(value)
 
     End Function
 End Module
