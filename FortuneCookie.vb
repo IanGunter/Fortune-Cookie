7e409333ff8b3cae94fb76aafd407d920460edb3
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-11-08 08:43:19 -0700

    see comments

--- FortuneCookie.vb
+++ FortuneCookie.vb
@@ -3,26 +3,18 @@
 'RCET 0265
 'Fall 2020
 'https://github.com/IanGunter/Fortune-Cookie.git
-
 Option Strict On
 Option Compare Text
 Option Explicit On
-
-
 Module FortuneCookie
-
     Sub Main()
         Dim randomNumber As Integer
-        Randomize()
+        Randomize() 'This should be in the random number sub - TJR
         Do
-
-
-
             Console.WriteLine("Press Enter to hear your future.")
             Console.ReadLine()
             'GetRandomNumber(1,2) sets the random number maximum and minimum values
             randomNumber = GetRandomNumber(1, 2)
-
             If randomNumber = 1 Then
                 Console.WriteLine("Fortune: Beware of creepy crawlers.")
             ElseIf randomNumber = 2 Then
@@ -30,31 +22,15 @@ Module FortuneCookie
             ElseIf randomNumber = 3 Then
                 Console.WriteLine("Fortune: You do not want to sell me death sticks. ")
             End If
-
             Console.ReadLine()
             Console.Clear()
-
         Loop
-
-
-
-
-
-
-
-
     End Sub
-
     'Function Below creates a randomly generated number between 1 and 3.
     Function GetRandomNumber(ByVal minimum As Integer, ByVal maximum As Integer) As Integer
         Dim value As Single
         'Rnd picks a number between 0 and 1.
         value = ((maximum - minimum + 1) * Rnd()) + minimum
         Return CInt(value)
-
     End Function
 End Module
-
-
-
-
